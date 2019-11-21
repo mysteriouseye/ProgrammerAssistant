d844fde62c68b7a16edfabf1af9080d493dcc7ed
mysteriouseye <v1200347@live.com>
2019-11-21 08:09:01 +0800

    fifth

--- client/src/main-process/index.js
+++ client/src/main-process/index.js
@@ -1,7 +1,12 @@
 const { ipcMain } = require('electron');
 const jsonfile = require('jsonfile');
+const config_name = `${__dirname}/../json/window.json`;
+jsonfile.readFile(config_name, (err, data) => {
+    if(err) throw err;
+    ipcMain.on('back-image', (event) => {
 
-jsonfile.readFile()
+    })
+})
 ipcMain.on('online-status-changed', (event, status) => {
     console.log(status);
 })
